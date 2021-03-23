8f3cec674eff1d4bbe80a1c32f75aefebd88facf
Documenter.jl <documenter@juliadocs.github.io>
2021-03-23 06:15:39 +0000

    build based on 2e66c75

--- dev/examples/spatial_transformation/SpatialTransformations.jl
+++ dev/examples/spatial_transformation/SpatialTransformations.jl
@@ -1,4 +1,5 @@
-using Images, ImageTransformations, TestImages, OffsetArrays
+using Images, TestImages, OffsetArrays
+# ImageTransformations is reexported by Images
 # load an example image
 img_source = testimage("lighthouse")
 
@@ -8,7 +9,10 @@ img_cropped = @view img_source[ :,floor(Int, 1/8*img_size[2]) : floor(Int, 7/8*i
 
 size(img_cropped)
 
-img_padded = PaddedView(ARGB(0, 0, 0, 0), OffsetArray(img_cropped, OffsetArrays.Origin(1, floor(Int, 1/8*img_size[2]))), axes(img_source))
+img_padded = PaddedView(
+    zero(eltype(img_source)),
+    OffsetArray(img_cropped, OffsetArrays.Origin(1, floor(Int, 1/8*img_size[2]))),
+    axes(img_source))
 
 img_square = imresize(img_source, (400, 400));
 img_small = imresize(img_source, ratio=1/4);
@@ -27,8 +31,8 @@ img_rescaled = imresize(img_source, new_size);
 mosaicview(img_source, img_rescaled; nrow=1)
 
 rescaled_both = restrict(img_source); # both side
-rescaled_height = restrict(img_source, 1); # height
-rescaled_width = restrict(img_source, 2); # width
+rescaled_height = restrict(img_source, 1); # dims=1, i.e., height dimension
+rescaled_width = restrict(img_source, 2); # dims=2, i.e., width dimension
 mosaicview(img_source, rescaled_both, rescaled_height, rescaled_width; nrow=1)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
